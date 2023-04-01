0e26269cf924d906418c46c484effa55eb0b9710
Henrique <curtishenrique10@gmail.com>
2023-04-01 01:56:43 -0300

    Portfolio com angular.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjetosComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
